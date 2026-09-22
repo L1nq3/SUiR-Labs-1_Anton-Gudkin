eddae8fadb10214fa3272fd05e865f9a31825e23
L1nq3 <anton.gudkin@icloud.com>
2026-09-22 20:07:59 +0300

    Update 2
    Исправил задание с анимацией

--- Temp_Lab.py
+++ Temp_Lab.py
@@ -1,22 +1,26 @@
 RED = '\u001b[41m'
 GREEN = '\u001b[42m'
 YELLOW = '\u001b[43m'
+BLUE = '\u001b[44m'
+PURPLE = '\u001b[45m'
+CIAN = '\u001b[46m'
 RESET = '\u001b[0m'
 WHITE = '\u001b[47m'
 
 import time
 import sys
 import os
+import math
 
 def Benin():
     pixel = ' '
     height = 10
     for i in range(height):
-        print(GREEN + pixel*6, end=' ')
+        print(GREEN + pixel*6, end=' ') #зеленая полоса 
         if height//2 > i:
-            print(YELLOW + pixel*20 + RESET)
+            print(YELLOW + pixel*20 + RESET) #желтая полоса первую половину
         else:
-            print(RED + pixel*20 + RESET)
+            print(RED + pixel*20 + RESET) #красная полоса вторую половину
 
 def Picture(n):
     COLOR = f'\u001b[{n}m'
@@ -38,14 +42,6 @@ def Picture(n):
         else:
             sector += 1
             mid_sector += 1
-
-def animation():
-    colors = [41, 42, 43, 44] 
-    while True:
-        for i in range(4):
-            Picture(colors[i])
-            time.sleep(1)
-            os.system("clear")
 
 def file_state():
     file = open('sequence.txt')
@@ -84,11 +80,125 @@ def graphics():
             add = ''
         print(add + str(x),end=' ')
 
+def Frame_vanila():
+    pixel = ' '
+    offset = 6
+    print(WHITE + offset*pixel + GREEN + 9*pixel + WHITE + offset*pixel + RESET)
+    for i in range(1, 3):
+        print(WHITE + (offset - i)*pixel + GREEN + 2*pixel + WHITE + (5 + 2*i)*pixel + GREEN + 2*pixel + WHITE + (offset-i)*pixel + RESET)
+    for _ in range(3):
+        print(WHITE + (offset-2)*pixel + GREEN + 2*pixel + WHITE + 9*pixel + GREEN + 2*pixel + WHITE + (offset-i)*pixel + RESET)
+    for i in range(2, 0, -1):
+        print(WHITE + (offset - i)*pixel + GREEN + 2*pixel + WHITE + (5 + 2*i)*pixel + GREEN + 2*pixel + WHITE + (offset-i)*pixel + RESET)
+    print(WHITE + offset*pixel + GREEN + 9*pixel + WHITE + offset*pixel + RESET)
+    for _ in range(2):
+        print(WHITE + (offset + 4)*pixel + GREEN + pixel + WHITE + (offset+4)*pixel + RESET)
+    print(WHITE + (offset)*pixel + GREEN + pixel*9 + WHITE + (offset)*pixel + RESET)
+
+def Frame_1():
+    pixel = ' '
+    offset = 6
+    print(RESET + offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for i in range(1, 3):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + (pixel*(2+i) + RED + pixel + RESET + pixel*(2+i)) + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    for n in range(3):
+        if n < 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + RED + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        elif n == 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (CIAN + pixel*4 + BLUE + pixel + CIAN + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        else:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + RED + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+    for i in range(2, 0, -1):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + (pixel*(2+i) + RED + pixel + RESET + pixel*(2+i)) + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    print(offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for _ in range(2):
+        print((offset + 4)*pixel + GREEN + pixel + RESET + (offset+4)*pixel)
+    print((offset)*pixel + GREEN + pixel*9 + RESET + (offset)*pixel)
+
+def Frame_2():
+    pixel = ' '
+    offset = 6
+    print(RESET + offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for i in range(1, 3):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + 2*(i-1)*pixel + CIAN + pixel + RESET + pixel*(7-2*i) + RED + pixel + RESET + 2*(i-1)*pixel + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    for n in range(3):
+        if n < 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*3 + CIAN + pixel + RESET + pixel + RED + pixel + RESET + pixel*3) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        elif n == 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + BLUE + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        else:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*3 + RED + pixel + RESET + pixel + CIAN + pixel + RESET + pixel*3) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+    for i in range(2, 0, -1):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + 2*(i-1)*pixel + RED + pixel + RESET + pixel*(7-2*i) + CIAN + pixel + RESET + 2*(i-1)*pixel +  GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    print(offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for _ in range(2):
+        print((offset + 4)*pixel + GREEN + pixel + RESET + (offset+4)*pixel)
+    print((offset)*pixel + GREEN + pixel*9 + RESET + (offset)*pixel)
+
+def Frame_3():
+    pixel = ' '
+    offset = 6
+    print(RESET + offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for i in range(1, 3):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + (pixel*(2+i) + CIAN + pixel + RESET + pixel*(2+i)) + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    for n in range(3):
+        if n < 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + CIAN + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        elif n == 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (RED + pixel*4 + BLUE + pixel + RED + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        else:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + CIAN + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+    for i in range(2, 0, -1):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + (pixel*(2+i) + CIAN + pixel + RESET + pixel*(2+i)) + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    print(offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for _ in range(2):
+        print((offset + 4)*pixel + GREEN + pixel + RESET + (offset+4)*pixel)
+    print((offset)*pixel + GREEN + pixel*9 + RESET + (offset)*pixel)
+
+def Frame_4():
+    pixel = ' '
+    offset = 6
+    print(RESET + offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for i in range(1, 3):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + 2*(i-1)*pixel + RED + pixel + RESET + pixel*(7-2*i) + CIAN + pixel + RESET + 2*(i-1)*pixel + GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    for n in range(3):
+        if n < 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*3 + RED + pixel + RESET + pixel + CIAN + pixel + RESET + pixel*3) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        elif n == 1:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*4 + BLUE + pixel + RESET + pixel*4) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+        else:
+            print((offset-2)*pixel + GREEN + 2*pixel + RESET + (pixel*3 + CIAN + pixel + RESET + pixel + RED + pixel + RESET + pixel*3) + GREEN + 2*pixel + RESET + (offset-2)*pixel)
+    for i in range(2, 0, -1):
+        print((offset - i)*pixel + GREEN + 2*pixel + RESET + 2*(i-1)*pixel + CIAN + pixel + RESET + pixel*(7-2*i) + RED + pixel + RESET + 2*(i-1)*pixel +  GREEN + 2*pixel + RESET + (offset-i)*pixel)
+    print(offset*pixel + GREEN + 9*pixel + RESET + offset*pixel)
+    for _ in range(2):
+        print((offset + 4)*pixel + GREEN + pixel + RESET + (offset+4)*pixel)
+    print((offset)*pixel + GREEN + pixel*9 + RESET + (offset)*pixel)
+
+def animation():
+    while True:
+        Frame_1() 
+        time.sleep(0.5)
+        os.system("clear")
+        Frame_2() 
+        time.sleep(0.5)
+        os.system("clear")
+        Frame_3() 
+        time.sleep(0.5)
+        os.system("clear")
+        Frame_4() 
+        time.sleep(0.5)
+        os.system("clear")
 # Benin()
 
-n = int(input('color number: '))
-Picture(n) # можно указать цвет
+# n = int(input('color number: '))
+# Picture(n) # можно указать цвет
 
-# animation()
+animation()
+
+# Frame_1()
+# Frame_2()
+# Frame_3()
+# Frame_4()
 
 # graphics()
